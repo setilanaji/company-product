2ad9e6e80fb9f764718a5139aa4331db3cf3aa9f
yudha setyaji <setyajiyudha299@gmail.com>
2023-07-05 09:03:18 +0700

    Initial Commit

---
+++ company catalog/company_catalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  company_catalogApp.swift
+//  company catalog
+//
+//  Created by Yudha S on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct company_catalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
